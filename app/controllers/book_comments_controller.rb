e38432325c4cbe921a6c7c0ca9f8be483044faa1
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-03-03 05:25:23 +0000

    [Finish]応用課 題6 修正後

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,21 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-   book = Book.find(params[:book_id])
-   comment = current_user.book_comments.new(book_comment_params)
-   comment.book_id = book.id
-   comment.save
-   redirect_to book_path(book)
+   @book = Book.find(params[:book_id])
+   @comment = current_user.book_comments.new(book_comment_params)
+   @comment.book_id = @book.id
+   @comment.save
   end
-  
+
   def destroy
-   BookComment.find(params[:id]).destroy
-   redirect_to book_path(params[:book_id])
+   @comment = BookComment.find(params[:id])
+   @comment.destroy
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
